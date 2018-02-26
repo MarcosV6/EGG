1863e94d506fd3440373d7e54da8d80ac2553de4
MarcosV6 <thisismarcos@gmail.com>
2018-02-25 20:14:46 -0500

    Update and rename tutorial1_contract.sol to EggTest.sol

--- Tutorial 1/EggTest.sol
+++ Tutorial 1/EggTest.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.18;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'EGG' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
+// Deployed to : 0x68B4C0488a1e72bD6d076241c20Fc1961dD14870
+// Symbol      : EGG
+// Name        : E.G.G
 // Total supply: 100000000
 // Decimals    : 18
 //
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract EGGToken is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -112,13 +112,13 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // ------------------------------------------------------------------------
     // Constructor
     // ------------------------------------------------------------------------
-    function FucksToken() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
+    function EGGToken() public {
+        symbol = "EGG";
+        name = "E.G.G";
         decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        _totalSupply = 1000000000000000000000000000000000;
+        balances[0x68B4C0488a1e72bD6d076241c20Fc1961dD14870] = _totalSupply;
+        Transfer(address(0), 0x68B4C0488a1e72bD6d076241c20Fc1961dD14870, _totalSupply);
     }
 
 
